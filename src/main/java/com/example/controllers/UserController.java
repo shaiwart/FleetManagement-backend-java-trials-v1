375c0326ec06f42c631a76a169610ad09549d1dc
shaiwart <133580233+shaiwart@users.noreply.github.com>
2023-07-15 17:32:40 +0530

    version1.4

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ public class UserController {
 	
 	
 	// add a user
+	@CrossOrigin(origins = "*")
 	@PostMapping(value="api/user") 
 	public void addUser(@RequestBody User user) {
 		userMan.addUser(user); 
@@ -25,7 +27,8 @@ public class UserController {
 	
 	
 	// get a user by id 
-	@GetMapping(value="api/{id}")
+	@CrossOrigin(origins = "*")
+	@GetMapping(value="api/user/{id}")
 	public User findById(@PathVariable int id) {
 		return userMan.findById(id); 
 	}
